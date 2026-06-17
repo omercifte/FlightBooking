a1860c71451ec3d180f45c6c42f1d565acda9458
omercifte <omer.cifte@icloud.com>
2026-06-17 13:11:11 +0300

    Add project files.

---
+++ FlightBooking/Areas/Admin/Controllers/BookingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BookingController : Controller
+    {
+        public IActionResult CreateBooking()
+        {
+            return View();
+        }
+
+        public IActionResult BookingList()
+        {
+            return View();
+        }
+    }
+}
---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultFlightStatusComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFlightStatusComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
